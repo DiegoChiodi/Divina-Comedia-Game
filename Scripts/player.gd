b77c3e04af01c23649d90b987e6b4cd1c60d3db3
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-10-05 17:53:09 -0300

    Multiple dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,49 +2,59 @@ extends Entity
 class_name Player
 
 #Dash ---------------
+var dashMax : int = 3
+var dash : int = dashMax
 var dashDuringDelay : float = 0.2
 var dashDuringWait : float = self.dashDuringDelay
+var dashSpeedMax : int = 3
 
 var dashDelay : float = 1.0
 var dashWait : float = self.dashDelay
 var dashPoss : bool = false # se é possivel dar dash
+var lockDash : bool = false
 @onready var sprite : ColorRect = $ColorRect
 
 var inDash : bool = false
 var speedDash : int = 1
 
 func _physics_process(delta: float) -> void:
-	self.dash(delta)
+	self.dashFunction(delta)
 	super.move(delta)
 
-func dash(delta) -> void:
+func dashFunction(delta) -> void:	
 	var attack : bool = Input.is_action_just_pressed("space") or Input.is_action_just_pressed("left_click")
 	
-	if attack && dashPoss:
+	if attack && dash > 0:
 		#Dando dash
 		self.inDash = true
 		self.dashDuringWait = 0.0
 		#cowdow do dash para usar denovo
-		self.dashPoss = false
+		self.dash -= 1
 		self.dashWait = 0.0
 	
+	#Dando dash
 	if self.dashDuringWait < self.dashDuringDelay:
 		self.dashDuringWait += delta
 	else:
 		self.inDash = false
+		self.lockDash = false
 	
 	if self.dashWait < self.dashDelay:
 		self.dashWait += delta
-		self.sprite.modulate = Color(1,1,0)
-	else:
-		self.dashPoss = true
-		self.sprite.modulate = Color(0,1,0)
+		self.sprite.modulate = Color(dashWait , dashWait, 0) #amarelo
+	elif dash < dashMax:
+		self.dash += 1
+		self.dashWait = 0.0
+	
+	if dash == dashMax:
+		self.sprite.modulate = Color(0,1,0) #Verde
 	
 	if self.inDash:
-		self.speedDash = 3
-		self.sprite.modulate = Color(1,0,0)
+		self.speedDash = dashSpeedMax
+		self.sprite.modulate = Color(1,0,0) #vermelho
 	else:
 		self.speedDash = 1
+	print(dash)
 
 func velocityTarget(delta) -> void:
 	velocity = velocity.lerp(move_direction * speed * speedDash, acceleration)
@@ -58,6 +68,8 @@ func groupsAdd() -> void:
 
 func move_directionTarget() -> void:
 	if inDash:
-		move_direction = (get_global_mouse_position() - self.global_position).normalized()
+		if !lockDash:
+			move_direction = (get_global_mouse_position() - self.global_position).normalized()
+			lockDash = true
 	else:
 		super.move_directionTarget()
