10f1dd733f4c523300297703ff69fff592608f7b
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-11-20 14:58:10 -0300

    revamp: restart rooom

--- Scripts/GameControl/RoomControl/roomContainer.gd
+++ Scripts/GameControl/RoomControl/roomContainer.gd
@@ -2,40 +2,42 @@ extends Node2D
 class_name RoomContainer
 
 var currentRoom : Level
-var previousMapId : int
+var previousMapId : int = -1
+var previousCurrentRoomTemp : int
 
 var inInit : bool = true
 var restart : bool = false
+var tentativa : int = 0
 
 func destroy_room() -> void:
 	self.currentRoom.call_deferred("queue_free")
 
 func load_room(path) -> void:
-	if !self.inInit and !self.restart:
-		self.previousMapId = self.currentRoom.map.id
+	#Guarda o id da cena atual que está preste a ser modificada antes que modifique
+	if self.currentRoom != null:
+		self.previousCurrentRoomTemp = self.currentRoom.map.id
 	
+	#Instancia a scena atual, configura e adiciona
 	self.currentRoom = load(path).instantiate()
-	
 	self.currentRoom.setup()
 	self.add_child(self.currentRoom)
 	
-	if !inInit:
-		var marker_name = game_manager.marker_names[previousMapId]
-		var marker : Marker2D = currentRoom.map.get_node(marker_name)
-		if marker != null:
-			var mark_position = marker.position
-			game_manager.player.set_deferred("position", mark_position)
-			game_manager.camera.setPosition(mark_position)
-		
-		if previousMapId == currentRoom.map.id:
-			var mark_position = self.currentRoom.map.get_node("player_spawn").position
-			game_manager.player.set_deferred("position", mark_position)
-			game_manager.camera.setPosition(mark_position)
-	else:
-		var mark_position = self.currentRoom.map.get_node("player_spawn").position
-		game_manager.player.set_deferred("position", mark_position)
-		game_manager.camera.setPosition(mark_position)
-	self.inInit = false
+	if self.inInit: #Inicío do jogo, previousMapId = null
+		posPlayerSpawn()
+		self.inInit = false
+		return
+	
+	#Se o jogo acabou de iniciar e previousMapId acabou de ser criado
+	#Estou no mapa inicial e acabei de vir dele
+	if self.restart and self.previousMapId == -1 and self.currentRoom.map.get_node("player_spawn") != null:
+		posPlayerSpawn()
+		self.restart = false
+		return
+	
+	#Para reiniciar no marker certo da scena anterior
+	if !self.restart:
+		self.previousMapId = previousCurrentRoomTemp
+	posPlayerPreviousRomm()
 	self.restart = false
 
 func change_room(path) -> void:
@@ -45,3 +47,15 @@ func change_room(path) -> void:
 func restartRoom(path: String) -> void:
 	self.restart = true
 	change_room(path)
+
+func posPlayerSpawn() -> void:
+	var mark_position = self.currentRoom.map.get_node("player_spawn").position
+	game_manager.player.set_deferred("position", mark_position)
+	game_manager.camera.setPosition(mark_position)
+
+func posPlayerPreviousRomm() -> void:
+	var marker_name = game_manager.marker_names[previousMapId]
+	var marker : Marker2D = self.currentRoom.map.get_node(marker_name)
+	var mark_position = marker.position
+	game_manager.player.set_deferred("position", mark_position)
+	game_manager.camera.setPosition(mark_position)
--- Scripts/GameControl/gameManager.gd
+++ Scripts/GameControl/gameManager.gd
@@ -38,12 +38,12 @@ func _process(_delta: float) -> void:
 		restartRoom()
 
 func init(main : Node2D):
-	self.camera.setup(player, null)
-	camera.limit_left = 0
-	camera.limit_top = 0
-	main.add_child(camera)
-	main.add_child(player)
-	roomContainer.load_room(level_paths[LevelID.FOREST_00])
+	self.camera.setup(self.player, null)
+	self.camera.limit_left = 0
+	self.camera.limit_top = 0
+	main.add_child(self.camera)
+	main.add_child(self.player)
+	self.roomContainer.load_room(self.level_paths[LevelID.FOREST_00])
 	main.add_child(roomContainer)
 	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -69,18 +69,25 @@ func windowsConf() -> void:
 	center_window(DisplayServer.screen_get_size())
 
 func start_shake(intensity: float, decay: float = 1.0) -> void:
-	camera.start_shake(intensity,decay)
+	self.camera.start_shake(intensity,decay)
 
-func restartRoom():
-	var actualMapId = roomContainer.currentRoom.map.id
-	self.player.queue_free()
+func restartRoom() -> void:
+	var actualMapId = self.roomContainer.currentRoom.map.id
+	
+	startPlayer()
+	self.camera.setup(self.player, null)
+	
+	self.roomContainer.restartRoom(self.level_paths[actualMapId])
+	self.camera.setPosition(self.player.position)
+
+func startPlayer() -> void:
+	if self.player != null:
+		self.player.queue_free()
 	
 	var newPlayer : Player = preload("res://Scene/player.tscn").instantiate()
 	self.player = newPlayer
 	self.add_child(self.player)
-	self.camera.setup(player, null)
-	roomContainer.restartRoom(level_paths[actualMapId])
-	 
 
 func change_room(_map_id) -> void:
-	roomContainer.call_deferred("change_room", level_paths[_map_id])
+	self.roomContainer.call_deferred("change_room", self.level_paths[_map_id])
+	self.camera.setPosition(self.player.position)
