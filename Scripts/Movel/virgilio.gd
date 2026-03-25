4d7d6a5059501f950100867b944dcb25bf77aa86
DiegoChiodi <dchiodigoncalves@gmail.com>
2026-03-25 17:49:31 -0300

    add: Virgilio create and move with animation

---
+++ Scripts/Movel/virgilio.gd
@@ -0,0 +1,11 @@
+extends AnimationEntity
+class_name Virgilio
+
+var trail_points : int = 0
+
+func _ready() -> void:
+	super._ready()
+
+func _process(_delta : float) -> void:
+	super._process(_delta)
+	self.direction = Vector2(0.6, 0.2)
